79ad01747d684383b25ed76386a7ce2bcd6ce49b
hardihuang <hardihuang@outlook.com>
2019-07-04 09:56:17 +0800

    update

--- code/unit_test/01_24L01/01_one_way_4_led_remote_control/liuyuchuan/liuyuchuan-nRF24L01-Transmit/liuyuchuan-nRF24L01-Transmit.ino
+++ code/unit_test/01_24L01/01_one_way_4_led_remote_control/liuyuchuan/liuyuchuan-nRF24L01-Transmit/liuyuchuan-nRF24L01-Transmit.ino
@@ -47,16 +47,20 @@ const byte address[6] = "00010";     //Byte of array representing the address. T
 //定义输入按键引脚
 int btnA = 2;
 int btnB = 3;
-int btnC = 4;
-int btnD = 5;
+int joystickA = A1;
+int joystickB = A0;
+int joystickC = A4;
+int joystickD = A5;
 
 //发送的数据结构
 struct dataStruct {
   unsigned long _micros;
   bool btnA;
   bool btnB;
-  bool btnC;
-  bool btnD;
+  int joyStick_A_X;
+  int joyStick_A_Y;
+  int joyStick_B_X;
+  int joyStick_B_Y;
 } myData;
 
 /*-----初始化函数-----*/
@@ -70,7 +74,18 @@ void setup() {
 void loop(){
   sendKey();
   delay(50);
-  
+  Serial.print(myData.btnA);
+  Serial.print(" ");
+  Serial.print(myData.btnB);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_A_X);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_A_Y);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_B_X);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_B_Y);
+  Serial.println(" ");
 }
 
 /*-----自定义函数-----*/
@@ -78,8 +93,10 @@ void loop(){
 void sendKey(){//读取并发送按键电平信号
   myData.btnA = !digitalRead(btnA);
   myData.btnB = !digitalRead(btnB);
-  myData.btnC = !digitalRead(btnC);
-  myData.btnD = !digitalRead(btnD);
+  myData.joyStick_A_X = analogRead(joystickA);
+  myData.joyStick_A_Y = analogRead(joystickB);
+  myData.joyStick_B_X = analogRead(joystickC);
+  myData.joyStick_B_Y = analogRead(joystickD);
   myData._micros = micros();
   radio.write(&myData, sizeof(myData));
 }
@@ -87,8 +104,11 @@ void sendKey(){//读取并发送按键电平信号
 void btnSetup(){//设置按键为输入并内部上拉
   pinMode(btnA, INPUT_PULLUP);
   pinMode(btnB, INPUT_PULLUP);
-  pinMode(btnC, INPUT_PULLUP);
-  pinMode(btnD, INPUT_PULLUP);  
+  pinMode(joystickA, INPUT);
+  pinMode(joystickB, INPUT);
+  pinMode(joystickC, INPUT);
+  pinMode(joystickD, INPUT);
+ 
 }
 
 void radioSetup(){//RF24L01发射端初始化
