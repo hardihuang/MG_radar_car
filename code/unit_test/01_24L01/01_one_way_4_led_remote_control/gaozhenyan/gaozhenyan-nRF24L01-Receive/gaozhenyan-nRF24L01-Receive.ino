4e6cfe05c23be4ef5cb2b8606045ba5202831853
hardihuang <hardihuang@outlook.com>
2019-07-04 17:17:31 +0800

    gaozhenyan code

---
+++ code/unit_test/01_24L01/01_one_way_4_led_remote_control/gaozhenyan/gaozhenyan-nRF24L01-Receive/gaozhenyan-nRF24L01-Receive.ino
@@ -0,0 +1,99 @@
+/*
+========================================
+  24L01无线通信 V0.2
+  高禛言
+========================================
+
+
+
+【24L01模块接线】(模块引脚参考资料文件夹图片)
+---------------------------------------
+	1 - GND
+	2 - VCC 连接3.3V
+	3 - CE 连接A5
+	4 - CSN 连接A4
+	5 - SCK 连接D13
+	6 - MOSI 连接D11
+	7 - MISO 连接D12
+	8 - UNUSED
+
+【按键连接】
+---------------------------------------
+
+*/
+
+
+//引入库文件
+#include <SPI.h>
+#include <RF24.h>
+
+//RF24L01配置
+RF24 radio(A5, A4); // CE, CSN
+const byte address[6] = "00010";
+
+int ledA = 5;
+int ledB = 2;
+int ledC = 3;
+int ledD = 4;
+
+//发送的数据结构
+struct dataStruct {
+  unsigned long _micros;
+  bool btnA;
+  bool btnB;
+  int joyStick_A_X;
+  int joyStick_A_Y;
+  int joyStick_B_X;
+  int joyStick_B_Y;
+} myData;
+
+void setup() {
+  Serial.begin(9600);
+  radioSetup();
+  ledSetup();
+}
+
+void loop(){
+  getData();
+}
+
+void getData(){//获取数据内容并打印在屏幕上
+  if (radio.available()){
+    radio.read( &myData, sizeof(myData) );
+	
+	//点亮对应LED
+
+  
+	
+	//打印内容
+  Serial.print(myData.btnA);
+  Serial.print(" ");
+  Serial.print(myData.btnB);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_A_X);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_A_Y);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_B_X);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_B_Y);
+  Serial.println(" ");
+	
+    delay(50);
+    
+  } 
+}
+
+void radioSetup(){//RF24L01接收端初始化
+  radio.begin();
+  radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
+  radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.startListening();              //This sets the module as receiver
+}
+
+void ledSetup(){
+	pinMode(ledA,OUTPUT);
+	pinMode(ledB,OUTPUT);
+	pinMode(ledC,OUTPUT);
+	pinMode(ledD,OUTPUT);
+}
---
+++ code/unit_test/01_24L01/01_one_way_4_led_remote_control/gaozhenyan/gaozhenyan-nRF24L01-Transmit/gaozhenyan-nRF24L01-Transmit.ino
@@ -0,0 +1,111 @@
+/*
+========================================
+  24L01无线通信 V0.2
+  高禛言
+========================================
+
+
+
+【24L01模块接线】(模块引脚参考资料文件夹图片)
+---------------------------------------
+	1 - GND
+	2 - VCC 连接3.3V
+	3 - CE 连接D5
+	4 - CSN 连接D4
+	5 - SCK 连接D13
+	6 - MOSI 连接D11
+	7 - MISO 连接D12
+	8 - UNUSED
+
+【按键连接】
+---------------------------------------
+	按钮A - 连接 D2
+	按钮B - 连接 D3
+	joystick_A - 连接 A0
+  joystick_B - 连接 A1
+  joystick_C - 连接 A2
+  joystick_D - 连接 A3
+*/
+
+
+//引入库文件
+#include <SPI.h>
+#include <RF24.h>
+
+//RF24L01配置
+RF24 radio(5, 4); // CE, CSN         
+const byte address[6] = "00010";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
+
+//定义输入按键引脚
+int btnA = 2;
+int btnB = 3;
+int joystickA = A0;
+int joystickB = A1;
+int joystickC = A2;
+int joystickD = A3;
+
+//发送的数据结构
+struct dataStruct {
+  unsigned long _micros;
+  bool btnA;
+  bool btnB;
+  int joyStick_A_X;
+  int joyStick_A_Y;
+  int joyStick_B_X;
+  int joyStick_B_Y;
+} myData;
+
+/*-----初始化函数-----*/
+void setup() {
+  Serial.begin(9600);
+  btnSetup();
+  radioSetup();
+}
+
+/*-----主循环函数-----*/
+void loop(){
+  sendKey();
+  delay(50);
+  Serial.print(myData.btnA);
+  Serial.print(" ");
+  Serial.print(myData.btnB);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_A_X);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_A_Y);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_B_X);
+  Serial.print(" ");
+  Serial.print(myData.joyStick_B_Y);
+  Serial.println(" ");
+}
+
+/*-----自定义函数-----*/
+
+void sendKey(){//读取并发送按键电平信号
+  myData.btnA = !digitalRead(btnA);
+  myData.btnB = !digitalRead(btnB);
+  myData.joyStick_A_X = analogRead(joystickA);
+  myData.joyStick_A_Y = analogRead(joystickB);
+  myData.joyStick_B_X = analogRead(joystickC);
+  myData.joyStick_B_Y = analogRead(joystickD);
+  myData._micros = micros();
+  radio.write(&myData, sizeof(myData));
+}
+
+void btnSetup(){//设置按键为输入并内部上拉
+  pinMode(btnA, INPUT_PULLUP);
+  pinMode(btnB, INPUT_PULLUP);
+  pinMode(joystickA, INPUT);
+  pinMode(joystickB, INPUT);
+  pinMode(joystickC, INPUT);
+  pinMode(joystickD, INPUT);
+ 
+}
+
+void radioSetup(){//RF24L01发射端初始化
+  radio.begin();                  //Starting the Wireless communication
+  radio.openWritingPipe(address); //Setting the address where we will send the data
+  radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.stopListening();          //This sets the module as transmitter
+}
